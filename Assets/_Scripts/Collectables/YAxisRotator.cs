d2d31a77872a763e44146fde79fc3bdf52cab7c0
Wayne Helley <waynehelley@hotmail.co.uk>
2015-08-06 17:07:17 +0100

    Round Cut Sapphire Added
    Round Cut Sapphire 3D Model and Rotator Script Added

---
+++ Assets/_Scripts/Collectables/YAxisRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YAxisRotator : MonoBehaviour {
+
+	public float speed = 1;
+
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate(new Vector3(0,0,30*speed)* Time.deltaTime);
+
+	}
+}
