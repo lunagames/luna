42d7f283780759bc69fc65ab6be57dfd56a673ab
BrandonFox <brandonfox@hotmail.co.uk>
2015-08-12 00:54:03 +0700

    SlowMotion script and zones reupload
    See if no errors this time. Also added a prefab for the slowmotion
    ability zones

--- Assets/_Scripts/Player/Abilities/Abilities.cs
+++ Assets/_Scripts/Player/Abilities/Abilities.cs
@@ -4,21 +4,37 @@ using System.Collections;
 public class Abilities : MonoBehaviour {
 	public GameObject player;
 	public float timeSlowSpeed = 0.5f;
+	public AbilityBar abilityBar;
+	public string activePower;
+	public float T_abilityCost = 10;
+	
+	private bool powerActivated = false;
 	void Awake(){
-		player = GameObject.FindGameObjectWithTag("Player");
+		player = this.gameObject;
+		abilityBar = FindObjectOfType<AbilityBar>();
 	}
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.F)){
-			Time.timeScale = timeSlowSpeed;
+		if(Input.GetKey(KeyCode.F)){
+			if(activePower == "TimeSlow"){
+				if(abilityBar.CurrentAbilityCharge>0){
+					Time.timeScale = timeSlowSpeed;
+					abilityBar.abilityCost = T_abilityCost;
+					powerActivated = true;
+				}
+			}
 		}
-		if(Input.GetKeyUp(KeyCode.F)){
-			Time.timeScale = 1;
+		if(Input.GetKeyUp(KeyCode.F)||activePower == null||!powerActivated){
+			if(powerActivated){
+				Time.timeScale = 1;
+				abilityBar.abilityCost = 0;
+				powerActivated = false;
+			}
 		}
 	}
 }
--- Assets/_Scripts/Player/Abilities/AbilityBar.cs
+++ Assets/_Scripts/Player/Abilities/AbilityBar.cs
@@ -2,71 +2,77 @@
 using System.Collections;
 
 public class AbilityBar : MonoBehaviour {
-
+	
 	public GUIStyle BlackBar;
-	public GUIStyle ExperienceBar;
+	public GUIStyle NoAbility;
+	public GUIStyle TimeSlow;
 	public GUIStyle TextField;
-
+	
 	public bool AbilityEnabled=false;
 	public bool AbilityDraining=false;
 	public float MaxAbilityCharge = 200;
 	public float CurrentAbilityCharge = 0;
-
+	public float abilityCost;
+	
+	public GUIStyle currentAbility;
+	void Start(){
+		currentAbility = NoAbility;
+	}
 	void Update(){
-
+		
 		if (CurrentAbilityCharge <= 0) {
 			AbilityEnabled = false;
 			CurrentAbilityCharge=0;
 		}
-
+		
 		if (AbilityDraining==true){
 			//Debug.Log("AbilityEnabled");
-			CurrentAbilityCharge-=Time.deltaTime*8;
+			CurrentAbilityCharge-=((Time.deltaTime*8)/Time.timeScale + (abilityCost/7));
 		}
 		
-			
-
-
-
-
+		
+		
+		
+		
+		
 	}
-
+	
 	void OnGUI()
 	{
 		//if (!Death) {  //If NOT death
-			
-			
+		
+		
 		//Experience bar
 		GUI.Box (new Rect (10, 10, 200, 10), "", BlackBar);  //BlackBar bar hehind the text
-		GUI.Box (new Rect (10, 10, CurrentAbilityCharge, 10), "", ExperienceBar);
+		GUI.Box (new Rect (10, 10, CurrentAbilityCharge, 10), "", currentAbility);
 		//GUI.Box (new Rect (20, 5, 200, 25), "XP: " + CurrentAbilityChange + "/" + MaxAbilityCharge, TextField);
-			
-			
-			
-			
-		}
-
+		
+		
+		
+		
+	}
+	
 	public void FillAbilityBar()
 	{
 		//Debug.Log("Ability Bar Fill");
 		CurrentAbilityCharge = MaxAbilityCharge;
-		AbilityEnabled = true;
+		AbilityEnabled = false;
 		AbilityDraining = false;
 	}
-
+	
 	public void AbilityBarDrainStart()
 	{
 		//Debug.Log("Ability Bar Fill");
 		AbilityDraining = true;
 	}
-
-
-
+	
+	
+	
 	public void EmptyAbilityBar()
 	{
 		CurrentAbilityCharge = 0;
 		AbilityEnabled = false;
 		AbilityDraining = false;
 	}
-
-	}
+	
+}
