a7a92a488530706e3913083234b8ef9033cad270
BrandonFox <brandonfox@hotmail.co.uk>
2015-08-13 03:14:14 +0700

    Fixed slow motion fireballs bug
    Fireballs will spawn with the same relative speed even if time has been
    slowed

--- Assets/_Scripts/Player/Abilities/FireballController.cs
+++ Assets/_Scripts/Player/Abilities/FireballController.cs
@@ -9,11 +9,18 @@ public class FireballController : MonoBehaviour {
 	private LunaCharacterController lunaCharacterController;
 	private Rigidbody2D rigidBody2D;
 	private float rScale;
-	
+	private float adjustedTime;
+
 	public float speed;
 	
 	// Use this for initialization
 	void Start () {
+		if(Time.timeScale == 1){
+			adjustedTime = Time.deltaTime;
+		}
+		else if(Time.timeScale != 1){
+			adjustedTime = Time.deltaTime / Time.timeScale;
+		}
 		rigidBody2D = GetComponent<Rigidbody2D>();
 		lunaCharacterController = GameObject.FindGameObjectWithTag("Player").GetComponent<LunaCharacterController>();
 		Quaternion newRotation;
@@ -21,7 +28,7 @@ public class FireballController : MonoBehaviour {
 		//if the character is facing to the right, shoot to the right
 		if(lunaCharacterController.m_FacingRight)
 		{
-			rigidBody2D.velocity = new Vector2(speed, 0f) * Time.deltaTime;
+			rigidBody2D.velocity = new Vector2(speed, 0f) * adjustedTime;
 			//rotate the fireball to face the correct direction
 			newRotation = Quaternion.Euler(0,0,0);
 			transform.rotation = newRotation;
@@ -29,7 +36,7 @@ public class FireballController : MonoBehaviour {
 		//if the character id facing left, shoot to the left
 		else if (!lunaCharacterController.m_FacingRight)
 		{
-			rigidBody2D.velocity = new Vector2(speed * -1f, 0f) * Time.deltaTime; 
+			rigidBody2D.velocity = new Vector2(speed * -1f, 0f) * adjustedTime; 
 			newRotation = Quaternion.Euler(0,180,0);
 			transform.rotation = newRotation;
 		}
