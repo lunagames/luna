0bed53533ebf12a1a721e44a010fe3d7317f0a79
brandonfox <brandonfox@hotmail.co.uk>
2015-08-19 02:07:01 +0700

    Added Double Jumps
    No animation yet as I am mostly unfamiliar with the animator controller
    but have added bools for anyone that knows how to

--- Assets/_Scripts/Player/LunaCharacterController.cs
+++ Assets/_Scripts/Player/LunaCharacterController.cs
@@ -23,7 +23,8 @@ namespace UnityStandardAssets._2D
         private Animator m_Anim;            // Reference to the player's animator component.
         private Rigidbody2D m_Rigidbody2D;
         public bool m_FacingRight = true;  // For determining which way the player is currently facing.
-
+		private bool m_doubleJumped = false;
+		private bool m_jumped = false;
 
 
         private void Awake()
@@ -40,18 +41,29 @@ namespace UnityStandardAssets._2D
         {
             m_Grounded = false;
 
+
             // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
             // This can be done using layers instead but Sample Assets will not overwrite your project settings.
             Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
             for (int i = 0; i < colliders.Length; i++)
             {
-                if (colliders[i].gameObject != gameObject)
+                if (colliders[i].gameObject != gameObject){
                     m_Grounded = true;
+					if(m_doubleJumped){
+						m_Anim.SetBool("DoubleJumped", false);
+						m_Anim.SetBool("HasDoubleJumped", false);
+					}
+					m_doubleJumped = false;
+
+
+				}
             }
             m_Anim.SetBool("Ground", m_Grounded);
-
+			m_Anim.SetBool("HasDoubleJumped", m_doubleJumped);
             // Set the vertical animation
             m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
+			m_jumped = !m_Grounded;
+			m_Anim.SetBool("Jumped", m_jumped);
         }
 
 
@@ -120,11 +132,19 @@ namespace UnityStandardAssets._2D
 
 		public void Jump(bool jump)
 		{
+			if(m_jumped && jump && !m_doubleJumped){
+				m_doubleJumped = true;
+				m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce - (m_Anim.GetFloat("vSpeed")*10)));
+				m_Anim.SetBool("DoubleJumped", true);
+				AudioSource audio = GetComponent<AudioSource>();
+				audio.Play();
+			}
 			// If the player should jump...
 			if (m_Grounded && jump && m_Anim.GetBool("Ground"))
 			{
 				// Add a vertical force to the player.
 				m_Grounded = false;
+				m_Anim.SetBool("Jumped", true);
 				m_Anim.SetBool("Ground", false);
 				m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
 				AudioSource audio = GetComponent<AudioSource>();
