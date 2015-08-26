a4fd857e64eafd024b014e80e11a82c04108246b
Sean O'Brien <ninetailsdevelopment@gmail.com>
2015-08-26 19:10:54 +0100

    Commiting work
    Had an issue with my work. For some reason it hadn't commited. Saws and
    check point stuff should now be included

---
+++ Assets/_Scripts/Environment/Obsticle Scripts/ObsticleRotation.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObsticleRotation : MonoBehaviour
+{
+
+    [Header("Rotation Values")]
+    public float rotationSpeed;
+
+    private bool reverseRotation;
+    public bool ReverseRotation
+    {
+        get { return reverseRotation; }
+        set { reverseRotation = value; }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        RotateObject();
+    }
+
+    public void RotateObject()
+    {
+        if (reverseRotation == true)
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+        
+    }
+
+
+}
